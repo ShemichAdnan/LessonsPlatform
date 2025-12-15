88e073758f282d5995ecdc4d15405d6b0750ffee
Adnan Semic <adnan.semic.23@size.ba>
2025-12-15 17:46:56 +0100

    Fixed port

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -18,7 +18,7 @@ app.use(helmet({
 }));
 
 app.use(cors({
-  origin: ['http://localhost:5173', 'http://localhost:5174'],
+  origin: ['http://localhost:5000', 'http://localhost:5001'],
   credentials: true
 }));
 
